e05b809756a3685a3ee79845312be70c6f52ae4f
Jose Ortiz <joseortizlack@outlook.com>
2017-11-26 12:10:36 -0600

    removed player and win image assets as they are no longer needed, and modified path to acces the image assets

--- js/load.js
+++ js/load.js
@@ -11,26 +11,24 @@ class loadState {
 		// Load all assets. The first parameter is the variable that 
 		// will point to the image, and the second parameter is the 
 		// image file itself.
-		game.load.image('player', 'assets/player.png');
-		game.load.image('win', 'assets/win.png');
-		game.load.image('startButton', 'assets/startButton.png');
-		game.load.image('tiledBackground', 'assets/tiledBackground.png');
-		game.load.image('A', 'assets/A.png');
-		game.load.image('E', 'assets/E.png');
-		game.load.image('I', 'assets/I.png');
-		game.load.image('O', 'assets/O.png');
-		game.load.image('U', 'assets/U.png');
-		game.load.image('questionMark1', 'assets/questionMark1.png');
-		game.load.image('questionMark2', 'assets/questionMark2.png');
-		game.load.image('questionMark3', 'assets/questionMark3.png');
-		game.load.image('questionMark4', 'assets/questionMark4.png');
-		game.load.image('speaker', 'assets/speaker.png');
-		game.load.image('escucha', 'assets/escucha.png');
-		game.load.image('avalancha', 'assets/avalancha.png');
-		game.load.image('restart', 'assets/restart.png');
-		game.load.image('lee', 'assets/lee.png');
-		game.load.image('home', 'assets/home.png');
-		game.load.image('background1', 'assets/background1.jpg');
+		game.load.image('startButton', 'assets/img/startButton.png');
+		game.load.image('tiledBackground', 'assets/img/tiledBackground.png');
+		game.load.image('A', 'assets/img/A.png');
+		game.load.image('E', 'assets/img/E.png');
+		game.load.image('I', 'assets/img/I.png');
+		game.load.image('O', 'assets/img/O.png');
+		game.load.image('U', 'assets/img/U.png');
+		game.load.image('questionMark1', 'assets/img/questionMark1.png');
+		game.load.image('questionMark2', 'assets/img/questionMark2.png');
+		game.load.image('questionMark3', 'assets/img/questionMark3.png');
+		game.load.image('questionMark4', 'assets/img/questionMark4.png');
+		game.load.image('speaker', 'assets/img/speaker.png');
+		game.load.image('escucha', 'assets/img/escucha.png');
+		game.load.image('avalancha', 'assets/img/avalancha.png');
+		game.load.image('restart', 'assets/img/restart.png');
+		game.load.image('lee', 'assets/img/lee.png');
+		game.load.image('home', 'assets/img/home.png');
+		game.load.image('background1', 'assets/img/background1.jpg');
 		game.load.audio('A', 'assets/audio/A.m4a');
 		game.load.audio('E', 'assets/audio/E.m4a');
 		game.load.audio('I', 'assets/audio/I.m4a');
