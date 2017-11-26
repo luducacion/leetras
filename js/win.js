b3e8c69e37b04c15ffb9f91da740a026ca98576d
Jose Ortiz <joseortizlack@outlook.com>
2017-11-26 11:41:48 -0600

    modified button position and win label position, text and font

--- js/win.js
+++ js/win.js
@@ -8,14 +8,14 @@ class winState {
 
 		var bg = game.add.tileSprite(game.world.centerX, game.world.centerY, game.world.width, game.world.height, 'tiledBackground');
 		bg.anchor.setTo(0.5,0.5);
-		var restartButton = game.add.button(game.world.centerX - 100 , game.world.centerY, 'restart', this.restartGame, this);
+		var restartButton = game.add.button(game.world.centerX - 120 , game.world.centerY, 'restart', this.restartGame, this);
 		restartButton.anchor.setTo(0.5,0.5);
 
-		var menuButton = game.add.button(game.world.centerX + 100 , game.world.centerY, 'home', this.goToMainMenu, this);
+		var menuButton = game.add.button(game.world.centerX + 120 , game.world.centerY, 'home', this.goToMainMenu, this);
 		
 		menuButton.anchor.setTo(0.5,0.5);
 
-		var winLabel = game.add.text(80, 80, '¡Ganaste!', {font: '50px Arial', fill: '#00FF00'});
+		var winLabel = game.add.text(game.world.centerX - 125, 80, '¡Ganaste!', {fontSize: '50px', fill: '#FFFFFF'});
 
 	}
 
