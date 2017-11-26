821a572b02bcd22a87bd606f021cc27ea66fb2a6
Jose Ortiz <joseortizlack@outlook.com>
2017-11-26 10:43:43 -0600

    added restart game and go back to menu buttons and functionality

--- js/win.js
+++ js/win.js
@@ -8,14 +8,26 @@ class winState {
 
 		var bg = game.add.tileSprite(game.world.centerX, game.world.centerY, game.world.width, game.world.height, 'tiledBackground');
 		bg.anchor.setTo(0.5,0.5);
-		var startButton = game.add.button(game.world.centerX, game.world.centerY, 'startButton', this.restart, this);
-		startButton.anchor.setTo(0.5,0.5);
+		var restartButton = game.add.button(game.world.centerX - 100 , game.world.centerY, 'startButton', this.restartGame, this);
+		restartButton.anchor.setTo(0.5,0.5);
+
+		var menuButton = game.add.button(game.world.centerX + 100 , game.world.centerY, 'startButton', this.goToMainMenu, this);
+		
+		menuButton.anchor.setTo(0.5,0.5);
+
+
 		var winLabel = game.add.text(80, 80, '¡Ganaste!', {font: '50px Arial', fill: '#00FF00'});
 
 	}
 
-	restart() {
-		
+	restartGame() {
+
+		this.levelData.score = null;
 		game.state.start('gameMaster', true, false, this.levelData);
+
+	}
+
+	goToMainMenu() {
+		game.state.start('menu');
 	}
 };
