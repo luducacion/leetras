e2c24fcf705f215c43f57e59ac17e2bfccd6495b
Jose Ortiz <joseortizlack@outlook.com>
2017-11-26 09:03:32 -0600

    added gameMaster cycle to lose and changed label name

--- js/lose.js
+++ js/lose.js
@@ -1,4 +1,10 @@
 class loseState {
+
+	init(levelData) {
+		console.log(levelData);
+		this.levelData = levelData;
+	}
+
 	create() {
 
 		var bg = game.add.tileSprite(game.world.centerX, game.world.centerY, game.world.width, game.world.height, 'tiledBackground');
@@ -6,11 +12,12 @@ class loseState {
 		var startButton = game.add.button(game.world.centerX, game.world.centerY, 'startButton', this.restart, this);
 		startButton.anchor.setTo(0.5,0.5);
 		
-		var winLabel = game.add.text(80, 80, 'Letra incorrecta, Vuelve a intentar', {font: '20px Arial', fill: '#00FF00'});
+		var loseLabel = game.add.text(80, 80, 'Letra incorrecta, Vuelve a intentar', {font: '20px Arial', fill: '#00FF00'});
 
 	}
 
 	restart() {
-		game.state.start('lee');
+		
+		game.state.start('gameMaster', true, false, this.levelData);
 	}
 };
