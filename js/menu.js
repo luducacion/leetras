0f31a51ae45b9396f675e43243fbc31282c99e08
Jose Ortiz <joseortizlack@outlook.com>
2017-11-26 14:08:42 -0600

    modified scale of objects

--- js/menu.js
+++ js/menu.js
@@ -5,12 +5,16 @@ class menuState {
 		var background = game.add.tileSprite(game.world.centerX, game.world.centerY, game.world.width, game.world.height, 'tiledBackground');
 		background.anchor.setTo(0.5,0.5);
 		
-		var nameLabel = game.add.text(game.world.centerX -160, 80, 'L E E T R A S', 
-									{ font: '50px Arial', fill: '#ffffff'});
+		var nameLabel = game.add.text(game.world.centerX, game.world.centerY - 80, 'L E E T R A S', { font: '50px Arial', fill: '#ffffff'});
+
+		nameLabel.anchor.setTo(0.5,0.5);
+		nameLabel.scale.setTo(scaleRatio, scaleRatio);
 
 		// Start Game 1
 		
 		var startGame1 = game.add.button(game.world.centerX - 100 , game.world.centerY, 'lee');
+		startGame1.anchor.setTo(0.5,0.5);
+		startGame1.scale.setTo(scaleRatio, scaleRatio);
 		startGame1.selectedGame = "lee";
 		startGame1.anchor.x = 0.5;
 		startGame1.anchor.y = 0.5;
@@ -21,6 +25,7 @@ class menuState {
 
 		// Start Game 2
 		var startGame2 = game.add.button(game.world.centerX + 100, game.world.centerY, 'escucha');
+		startGame2.scale.setTo(scaleRatio, scaleRatio);
 		startGame2.selectedGame = "escucha";
 		startGame2.anchor.x = 0.5;
 		startGame2.anchor.y = 0.5;
@@ -29,6 +34,7 @@ class menuState {
 
 		// Start Game 3
 		var startGame3 = game.add.button(game.world.centerX, game.world.centerY + 100, 'avalancha');
+		startGame3.scale.setTo(scaleRatio, scaleRatio);
 		startGame3.selectedGame = "avalancha";
 		startGame3.anchor.x = 0.5;
 		startGame3.anchor.y = 0.5;
