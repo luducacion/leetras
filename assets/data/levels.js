0d72ef9d3f57408a296182960e52f1cb35187608
Jose Ortiz <joseortizlack@outlook.com>
2017-11-26 10:32:34 -0600

    added score to level Data

--- assets/data/levels.js
+++ assets/data/levels.js
@@ -2,19 +2,22 @@ var levels= {
     "escucha" : [
         {
             "numberOfLetters": 5,
-            "availableLetters": ["A","E","I","O","U"]
+            "availableLetters": ["A","E","I","O","U"],
+            "score" : null
         }
     ],
     "lee" : [
         {
             "numberOfLetters": 5,
-            "availableLetters": ["A","E","I","O","U"]
+            "availableLetters": ["A","E","I","O","U"],
+            "score": null
         }
     ],
     "avalancha" : [
         {
             "numberOfLetters": 5,
-            "availableLetters": ["A","E","I","O","U"]
+            "availableLetters": ["A","E","I","O","U"],
+            "score": null
         }
     ]
 };
